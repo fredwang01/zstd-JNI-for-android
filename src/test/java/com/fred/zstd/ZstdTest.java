65116be8cbc6e4bd18cf0495e8a0d3db03265a42
azurelaker <terranazure@hotmail.com>
2017-01-13 11:39:11 +0800

    add test file

---
+++ src/test/java/com/fred/zstd/ZstdTest.java
@@ -0,0 +1,50 @@
+package com.fred.zstd;
+
+
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Arrays;
+
+public class ZstdTest {
+    private static final String TAG = "ZstdTest";
+
+    public boolean test(byte[] original) {
+        ByteArrayOutputStream compressedBytes = new ByteArrayOutputStream();
+        ByteArrayOutputStream uncompressedBytes = new ByteArrayOutputStream();
+
+        Zstd zstd = Zstd.getInstance();
+        // compress status
+        int ret = zstd.cinit();
+        if (ret != 0) {
+            Log.e(TAG, "cinit error.");
+            return false;
+        }
+        byte[] bytes = zstd.update(original);
+        if (bytes.length > 0) {
+            compressedBytes.write(bytes, 0, bytes.length);
+        }
+        bytes = zstd.end();
+        if (bytes.length > 0) {
+            compressedBytes.write(bytes, 0, bytes.length);
+        }
+
+        // uncompress status
+        ret = zstd.dinit();
+        if (ret != 0) {
+            Log.e(TAG, "dinit error.");
+            return false;
+        }
+        bytes = zstd.update(compressedBytes.toByteArray());
+        if (bytes.length > 0) {
+            uncompressedBytes.write(bytes, 0, bytes.length);
+        }
+        bytes = zstd.end();
+        if (bytes.length > 0) {
+            uncompressedBytes.write(bytes, 0, bytes.length);
+        }
+
+        // compare the original with the uncompressed data. should be equal.
+        return Arrays.equals(original, uncompressedBytes.toByteArray());
+    }
+}
